dd2d18e9bb936d80eca925f4c80b16f491cae16d
Heart Wirunrom <heart569522@gmail.com>
2024-07-26 19:52:52 +0700

    init project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/themes/theme-provider";
+import { anuphan } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={anuphan.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          // disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Anuphan } from "next/font/google";
+
+export const anuphan = Anuphan({
+    subsets: ["thai"],
+    weight: ["100", "200", "300", "400", "500", "600", "700"],
+    display: "swap",
+    adjustFontFallback: false,
+});
